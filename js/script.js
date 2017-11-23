4a3d4cacc2fd9f9497f1ee91cb29841015538fe1
Дмитрий Максимов <dmitri-maksimov@yandex.ru>
2017-11-23 16:39:32 +0300

    завершены модальные окна (js)

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var modalWriteUs = document.getElementById("modal-write-us-id");
+var openWriteUs = document.getElementById("modal-write-us-open");
+var closeWriteUs = document.getElementsByClassName("modal-close")[0];
+
+var modalMap = document.getElementById("modal-map-id");
+var openMap = document.getElementById("modal-map-open");
+var closeMap = document.getElementsByClassName("modal-close")[1];
+
+openWriteUs.onclick = function () {
+	modalWriteUs.style.display = "block";
+}
+
+closeWriteUs.onclick = function () {
+	modalWriteUs.style.display = "none";
+}
+
+window.onclick = function () {
+	if(event.target == modalWriteUs) {
+		modalWriteUs.style.display = "none";
+	}
+}
+
+openMap.onclick = function () {
+	modalMap.style.display = "block";
+}
+
+closeMap.onclick = function () {
+	modalMap.style.display = "none";
+}
+
+window.onclick = function () {
+	if(event.target == modalWriteUs) {
+		modalMap.style.display = "none";
+	}
+}
