fbcd60b90462e34cca7b187be3fed75c9f564009
Дмитрий Максимов <dmitri-maksimov@yandex.ru>
2017-11-29 14:02:01 +0300

    проблема с ie11 в подвале

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//  открытие модальных окон
 var modalWriteUs = document.getElementById("modal-write-us-id");
 var openWriteUs = document.getElementById("modal-write-us-open");
 var closeWriteUs = document.getElementsByClassName("modal-close")[0];
@@ -6,29 +7,31 @@ var modalMap = document.getElementById("modal-map-id");
 var openMap = document.getElementById("modal-map-open");
 var closeMap = document.getElementsByClassName("modal-close")[1];
 
-openWriteUs.onclick = function handlerWriteUs () {
+openWriteUs.onclick = function() {
+	event.preventDefault();
 	modalWriteUs.style.display = "block";
 }
 
-closeWriteUs.onclick = function () {
+closeWriteUs.onclick = function() {
 	modalWriteUs.style.display = "none";
 }
 
-window.onclick = function () {
+window.onclick = function() {
 	if(event.target == modalWriteUs) {
 		modalWriteUs.style.display = "none";
 	}
 }
 
-openMap.onclick = function handlerMap () {
+openMap.onclick = function handlerMap() {
+	event.preventDefault();
 	modalMap.style.display = "block";
 }
 
-closeMap.onclick = function () {
+closeMap.onclick = function() {
 	modalMap.style.display = "none";
 }
 
-window.onclick = function () {
+window.onclick = function() {
 	if(event.target == modalWriteUs) {
 		modalMap.style.display = "none";
 	}
